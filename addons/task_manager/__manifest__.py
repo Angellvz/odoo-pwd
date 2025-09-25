81f32def04bd6342a1a3b6060f57b80d5bf59bb2
Angellvz <luis.ventura@tecsup.edu.pe>
2025-09-25 21:23:22 +0000

    2

---
+++ addons/task_manager/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Task Manager",
+    "version": "1.0",
+    "summary": "Gestor simple de tareas para pruebas",
+    "category": "Productivity",
+    "author": "Tu Nombre",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/task_menu.xml",
+        "views/task_views.xml",
+        "report/task_report.xml"
+    ],
+    "application": True
+}
